55ab0f49a18eaef19afdf7a677b90eac69c17ed4
Manabu Nagaoka <manabunagaoka@gmail.com>
2024-12-20 01:50:50 +0000

    Simplify Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,12 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
@@ -14,21 +15,7 @@ const config: Config = {
       },
     },
   },
-  safelist: [
-    'bg-gray-900',
-    'text-gray-100',
-    'bg-gray-800',
-    'p-4',
-    'shadow-lg',
-    'z-50',
-    'text-2xl',
-    'font-bold',
-    'text-center',
-    'text-white',
-    'pt-16',
-    // Add any other Tailwind classes you're using
-  ],
   plugins: [],
-};
+}
 
-export default config;
+export default config
